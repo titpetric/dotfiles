a31f207bf5e1b7bc06bd949ffdfda0928f7ed77a
Tit Petric <tit.petric@monotek.net>
2022-03-16 11:16:16 +0100

    print entrypoint as it's added into bash_aliases, fix ssh to enable sudo -s

--- .bash_aliases
+++ .bash_aliases
@@ -1,3 +1,4 @@
 for entrypoint in ~/.dotfiles/enabled/*.sh; do
+	echo $entrypoint
 	. $entrypoint
 done
--- enabled/ssh-agent.sh
+++ enabled/ssh-agent.sh
@@ -1,6 +1,14 @@
+# start ssh-agent for local users only
+if [ "$USER" == "root" ]; then
+	return
+fi
+# start ssh-agent if not already started
 if [ ! -S ~/.ssh/ssh_auth_sock ]; then
-  eval `ssh-agent`
-  ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+	eval `ssh-agent`
+	ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+	# add the local users identity
+	ssh-add
 fi
 export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+# print added identity
 ssh-add -l
