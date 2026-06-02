ff02d677f31c96f44f2c32cb9a6f5df534b6e9f0
Tit Petric <black@titpetric.com>
2026-06-02 17:27:16 +0200

    ssh-agent: add some identity handling changes

--- enabled/ssh-agent.sh
+++ enabled/ssh-agent.sh
@@ -1,17 +1,20 @@
+#!/bin/bash
 # start ssh-agent for local users only
 if [ "$USER" == "root" ]; then
 	return
 fi
 
-SOCKETFILE="/tmp/ssh_auth_sock_$USER"
+# local user agent.sock symlink
+agent_link="$HOME/.ssh/agent.sock"
 
-# start ssh-agent if not already started
-if [ ! -f "$SOCKETFILE" ]; then
-	eval `ssh-agent`
-	ln -sf "$SSH_AUTH_SOCK" $SOCKETFILE
-	# add the local users identity
-	ssh-add
+if ! SSH_AUTH_SOCK="$agent_link" ssh-add -l >/dev/null 2>&1; then
+	eval "$(ssh-agent -s)" >/dev/null
+	ln -sf "$SSH_AUTH_SOCK" "$agent_link"
+
+	ssh-add || echo "No identities added."
 fi
-export SSH_AUTH_SOCK=$SOCKETFILE
+
+export SSH_AUTH_SOCK="$agent_link"
+
 # print added identity
 ssh-add -l
